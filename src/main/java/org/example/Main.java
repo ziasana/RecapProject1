423f40ad28cb4cfba58aeb3e7c30b7fb93706f3b
ziasana <zs.hossainy@gmail.com>
2025-10-14 16:20:59 +0200

    main method updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
           System.out.println("Hello and welcome!");
+          System.out.println("Text to add in a push");
     }
 }
