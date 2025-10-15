455809bed9e1be0a54603998b21c2986242318e2
ziasana <zs.hossainy@gmail.com>
2025-10-15 13:30:26 +0200

    password invalid and valid data creted

--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -10,52 +9,52 @@ class PasswordValidatorTest {
 
 
     @ParameterizedTest
-    @CsvFileSource (resources = "/password-data.txt")
+    @CsvFileSource (resources = "/password-valid-data.txt")
     void hasSpace_ReturnFalseWhenSpaceExist(String password) {
         assertFalse(PasswordValidator.hasSpace(password));
     }
 
 
     @ParameterizedTest
-    @CsvFileSource (resources = "/password-data.txt")
+    @CsvFileSource (resources = "/password-valid-data.txt")
     void hasMinLength_ReturnFalseWhenLengthIsSmallerThen8(String password) {
         assertTrue(PasswordValidator.hasMinLength(password, 8));
     }
 
     @ParameterizedTest
-    @CsvFileSource (resources = "/password-data.txt")
+    @CsvFileSource (resources = "/password-valid-data.txt")
     void containsDigit_ReturnTrueWhenContainsAtLeastOneDigit(String password) {
         assertTrue(PasswordValidator.containsDigits(password));
     }
 
     @ParameterizedTest
-    @CsvFileSource (resources = "/password-data.txt")
+    @CsvFileSource (resources = "/password-valid-data.txt")
     void containsUpperAndLower_ReturnTrueWhenContainsBothLowerAndUpper(String password) {
         assertTrue(PasswordValidator.containsUpperAndLower(password));
     }
 
     @ParameterizedTest
-    @CsvFileSource (resources = "/password-data.txt")
+    @CsvFileSource (resources = "/password-valid-data.txt")
     void isCommonPassword_ReturnFalseWhenContainsCommonPassword(String password) {
         assertFalse(PasswordValidator.isCommonPassword(password));
     }
 
     @ParameterizedTest
-    @CsvFileSource (resources = "/password-data.txt")
+    @CsvFileSource (resources = "/password-valid-data.txt")
     void containsTwoSpecialChar_ReturnTrueWhenContainsTwoSpecialChar(String password) {
         String allowedChar="#$%&?@()";
         assertTrue(PasswordValidator.containsTwoSpecialChar(password, allowedChar));
     }
 
     @ParameterizedTest
-    @CsvFileSource (resources = "/password-data.txt")
+    @CsvFileSource (resources = "/password-valid-data.txt")
     void containsTwoDigits_ReturnTrueWhenContainsAtLeastTwoDigits(String password) {
         assertTrue(PasswordValidator.containsTwoDigits(password));
     }
 
 
     @ParameterizedTest
-    @CsvFileSource (resources = "/password-data.txt")
+    @CsvFileSource (resources = "/password-valid-data.txt")
     void isValid_ReturnTrueWhenAllChecksPassed(String password) {
 
         assertAll("password validation",
